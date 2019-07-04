65f024b49056aefc9ae64af70511f1f7dc1e7736
RomeroGabriel <gabrielromerods@hotmail.com>
2019-07-04 17:44:13 -0300

    Terminando API

--- app.js
+++ app.js
@@ -29,22 +29,6 @@ app.use('/', indexRouter);
 app.use('/teacher', teachersRouter);
 app.use('/orientation', orientationRouter);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 mongoose.connect('mongodb+srv://romero:romero123@cluster0-sbrv8.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'ERRO CONEXÃO BD'));
